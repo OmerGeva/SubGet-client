d252be9efd4fcb819d533233682ccf7d36641c5b
Omer Geva <ogeva97@gmail.com>
2022-05-16 18:18:53 +0300

    first

---
+++ app/src/main/java/com/example/subget/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.subget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
