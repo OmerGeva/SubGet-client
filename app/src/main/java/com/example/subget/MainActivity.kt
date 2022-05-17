cb76e1baa8425f58cf06c3a0592ff0b6c9d674de
Daniel Itkes <danielitkes@gmail.com>
2022-05-17 10:42:13 +0300

    Added All Fragments and made Nav Graph

--- app/src/main/java/com/example/subget/MainActivity.kt
+++ app/src/main/java/com/example/subget/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.subget
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.subget.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        }
+
 }
