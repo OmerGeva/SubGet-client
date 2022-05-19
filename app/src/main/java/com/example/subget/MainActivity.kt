6f23aa5be33e488b95635c5c108d674ca6a239f6
Daniel Itkes <danielitkes@gmail.com>
2022-05-19 21:04:15 +0300

    Brought back Alon's Fragment method

--- app/src/main/java/com/example/subget/MainActivity.kt
+++ app/src/main/java/com/example/subget/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.subget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.subget.databinding.ActivityMainBinding
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.subget.favorites.FavoritesFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -34,5 +36,17 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+//        binding.detailBtn.setOnClickListener {
+//            replaceFragment(DetailsFragment())
+//        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, fragment)
+        transaction.commit()
+    }
+
 }
