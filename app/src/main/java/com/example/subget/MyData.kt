ba5079b65c641c2f5ef721c0239d5635a60a477e
Omer Geva <ogeva97@gmail.com>
2022-05-21 10:49:06 +0300

    feat: listings are now fetched in ListingsFragment and displayed in <a temporary> TextView to present the data. TO DO: Move this data to a recycleview with listing cards

---
+++ app/src/main/java/com/example/subget/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.subget
+
+class MyData : ArrayList<MyDataItem>()
---
+++ app/src/main/java/com/example/subget/MyDataItem.kt
@@ -0,0 +1,13 @@
+package com.example.subget
+
+data class MyDataItem(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val address: String,
+    val phone_number: String,
+    val image: String,
+    val washing_machine: Boolean,
+    val pet_allowed: Boolean,
+    val price: Int
+)
