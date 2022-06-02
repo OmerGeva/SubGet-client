801e6f819e4f2980aec133cc0c9db777f498533a
Daniel Itkes <danielitkes@gmail.com>
2022-06-02 15:16:01 +0300

    Added Favorite table to Room

--- app/src/main/java/com/example/subget/app_data/local_db/AppDatabase.kt
+++ app/src/main/java/com/example/subget/app_data/local_db/AppDatabase.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.subget.app_data.models.Favorite
 import com.example.subget.app_data.models.Listing
 
-@Database(entities = [Listing::class], version = 1, exportSchema = false)
+@Database(entities = [Listing::class, Favorite::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun appDatabaseDao(): DatabaseDAO
