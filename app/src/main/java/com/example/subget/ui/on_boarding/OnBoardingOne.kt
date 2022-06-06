b6ca11fe119c725819113884233410436757df09
Daniel Itkes <danielitkes@gmail.com>
2022-06-06 22:04:36 +0300

    fix

--- app/src/main/java/com/example/subget/ui/on_boarding/OnBoardingOne.kt
+++ app/src/main/java/com/example/subget/ui/on_boarding/OnBoardingOne.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.subget.R
 import com.example.subget.databinding.FragmentListingsBinding
 import com.example.subget.databinding.FragmentOnBoardingOneBinding
@@ -23,7 +24,7 @@ class OnBoardingOne : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentOnBoardingOneBinding.inflate(inflater, container, false)
 
-        val viewPager = activity?.findViewById<ViewPager>(R.id.view_pager)
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.view_pager)
 
         binding.next.setOnClickListener {
             viewPager?.currentItem = 1
